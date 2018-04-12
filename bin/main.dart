340f9d54774a8f4a26bf26967dcab7996ed8e43f
Nikita Johansson <nikjoh131@gmail.com>
2018-04-12 18:08:30 +0200

    Starting project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Flutter_Circus/Flutter_Circus.dart' as Flutter_Circus;
+
+main(List<String> arguments) {
+  print('Hello world: ${Flutter_Circus.calculate()}!');
+}
---
+++ test/Flutter_Circus_test.dart
@@ -0,0 +1,8 @@
+import 'package:Flutter_Circus/Flutter_Circus.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
